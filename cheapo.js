8647ad224f81be3ab3d8441299c620ca6429ffff
AundreE <32345683+AundreE@users.noreply.github.com>
2017-12-03 21:25:58 -0600

    php

---
+++ cheapo.js
@@ -0,0 +1,103 @@
+/* global $ */
+ $(document).ready(function(){
+     
+     
+     //login
+     $("#logbtn").on('click', function(event){
+         
+         event.preventDefault();
+         
+         var name = $("#username").val();
+         var pass = $("#password").val();
+         
+         var dat = "logname="+name+"&logpass="+pass;
+         
+         var lxmlhttp = new XMLHttpRequest();
+         
+         lxmlhttp.onreadystatechange = function() {
+             if (this.readyState == 4){
+                     if (this.status == 200) {
+                         //make navbar visible if successful login
+                         if (lxmlhttp.responseText == "User Found"){
+                             //$("#navbar").show();
+                             $("#main").load("home.html");
+                             //getmail();
+                         }
+                         else{
+                             $("#status").text("User Info Not Found! Check Login Info!");
+                         }
+                     }
+                     else{
+                         $("#status").text("Some Unknown Error Occured");
+                     }
+             }
+         };
+         
+         lxmlhttp.open("POST", "cheapo.php", true);
+         lxmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+         lxmlhttp.send(dat);
+     });
+     
+     //Nagivate using AJAX
+     $("#navbar ul li a").on('click', function(event){
+         
+         var logout = function(){
+             var xxmlhttp = new XMLHttpRequest();
+             
+             var dat ="logout=true";
+         
+             xxmlhttp.onreadystatechange = function() {
+                 if (this.readyState == 4){
+                     if (this.status == 200) {
+                         window.location.href = "/";
+                     }
+                 }
+             };
+         
+             xxmlhttp.open("POST", "cheapo.php", true);
+             xxmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+             xxmlhttp.send(dat);
+         }
+         
+         event.preventDefault();
+         var page = $(this).attr("href");
+         
+         if (page == "index.html"){
+             logout();
+         }
+         
+         else if(page == "home.html"){
+             $("#main").load(page);
+             getmail();
+         }
+         else{
+             $("#main").load(page);
+         }
+     });
+     
+     function getmail(){
+         //handle getting mail
+         var link = 'cheapo.php?getmail=true';
+         
+         $.ajax(link,{
+             method: 'GET' 
+         }).done(function(res){
+             $("#mail").html(res);
+             $('.recv').hide();
+          
+              $('.showbutton').on('click', function(){
+                  $(this).prev().slideToggle(400);
+                 readMail($(this).parent());
+              });
+              
+          }).fail(function(){
+              $("#mail").html("<p>Some Error Occured</p>");
+          });
+      }
+      
+
+     function readMail(div){
+          //handle reading message here
+        $(div).attr('class', 'mail read');
+     }
+  }); 
